99bf952d90beea4cb9efa2b5f42b4beb24bbd99d
Mark Zuber <mark@zube.com>
2019-03-29 20:11:38 -0700

    timer colors

--- src/HomeControl.StreamDeck/StreamDeckService.cs
+++ src/HomeControl.StreamDeck/StreamDeckService.cs
@@ -15,6 +15,8 @@ namespace HomeControl.StreamDeck
         private readonly ILogger _logger;
         private readonly IOptions<StreamDeckConfig> _config;
         private readonly IStreamDeckController _streamDeckController;
+        private Timer _timer;
+        private readonly Random _random = new Random();
 
         public StreamDeckService(ILogger<StreamDeckService> logger, IOptions<StreamDeckConfig> config)
         {
@@ -23,16 +25,30 @@ namespace HomeControl.StreamDeck
             _streamDeckController = StreamDeckFactory.CreateDeck();
         }
 
+        private void DoWork(object state)
+        {
+            for (int i = 0; i < _streamDeckController.NumKeys; i++)
+            {
+                var bytes = new byte[3];
+                _random.NextBytes(bytes);
+                _streamDeckController.FillColor(i, bytes[0], bytes[1], bytes[2]);
+            }
+        }
+
         public Task StartAsync(CancellationToken cancellationToken)
         {
             _logger.LogInformation("Starting StreamDeckService");
             _streamDeckController.FillAllKeysWithColor(0, 255, 0);
+
+            _timer = new Timer(DoWork, null, TimeSpan.Zero, TimeSpan.FromSeconds(1));
+
             return Task.CompletedTask;
         }
 
         public Task StopAsync(CancellationToken cancellationToken)
         {
             _logger.LogInformation("Stopping StreamDeckService.");
+            _timer?.Change(Timeout.Infinite, 0);
             _streamDeckController.ClearAllKeys();
             return Task.CompletedTask;
         }
@@ -40,6 +56,7 @@ namespace HomeControl.StreamDeck
         public void Dispose()
         {
             _logger.LogInformation("Disposing....");
+            _timer?.Dispose();
             _streamDeckController.Dispose();
         }
     }
