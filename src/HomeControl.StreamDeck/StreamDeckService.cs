715a64afd85e205d277cd912dc2b4a6c6bd0083b
Mark Zuber <mark@zube.com>
2019-03-30 19:40:00 -0700

    add base taskhostedservice and get images async

--- src/HomeControl.StreamDeck/StreamDeckService.cs
+++ src/HomeControl.StreamDeck/StreamDeckService.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using HomeControl.StreamDeck.Api;
+using HomeControl.StreamDeck.Common;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
@@ -12,12 +14,11 @@ using Zube.StreamDeck;
 
 namespace HomeControl.StreamDeck
 {
-    public class StreamDeckService : IHostedService, IDisposable
+    public class StreamDeckService : TaskHostedService, IDisposable
     {
         private readonly ILogger _logger;
         private readonly IOptions<StreamDeckConfig> _config;
         private readonly IStreamDeckController _streamDeckController;
-        private Timer _timer;
         private readonly Random _random = new Random();
         private readonly IStreamDeckApi _streamDeckApi;
 
@@ -35,60 +36,59 @@ namespace HomeControl.StreamDeck
 
         private void OnStreamDeckKeyPressed(object sender, StreamDeckKeyChangedEventArgs e)
         {
-            _logger.LogInformation($"StreamDeck Key pressed: {e.KeyIndex} -> {e.KeyOn}");
             if (e.KeyOn)
             {
+                _logger.LogInformation($"StreamDeck Key pressed: {e.KeyIndex}");
                 _streamDeckApi.PressKey(e.KeyIndex);
             }
-        }
-
-        private void DoWork(object state)
-        {
-            for (int i = 0; i < _streamDeckController.NumKeys; i++)
-            {
-                //var bytes = new byte[3];
-                //_random.NextBytes(bytes);
-                //_streamDeckController.FillColor(i, bytes[0], bytes[1], bytes[2]);
-
-                try
-                {
-                    var imageBytes = _streamDeckApi.GetImageForKey(i);
-                    using (var ms = new MemoryStream(imageBytes))
-                    {
-                        _streamDeckController.SetImage(i, ms);
-                    }
-                }
-                catch (Exception ex)
-                {
-                    _logger.LogError(ex, "Failure calling GetImageForKey");
-                }
-            }
-        }
-
-        public Task StartAsync(CancellationToken cancellationToken)
-        {
-            _logger.LogInformation("Starting StreamDeckService");
-            _streamDeckController.FillAllKeysWithColor(0, 255, 0);
-
-            _timer = new Timer(DoWork, null, TimeSpan.Zero, TimeSpan.FromSeconds(10));
-
-            return Task.CompletedTask;
-        }
-
-        public Task StopAsync(CancellationToken cancellationToken)
-        {
-            _logger.LogInformation("Stopping StreamDeckService.");
-            _timer?.Change(Timeout.Infinite, 0);
-            _streamDeckController.ClearAllKeys();
-            return Task.CompletedTask;
         }
 
         public void Dispose()
         {
             _logger.LogInformation("Disposing....");
-            _timer?.Dispose();
             _streamDeckController.KeyPressed += OnStreamDeckKeyPressed;
             _streamDeckController.Dispose();
+        }
+
+        private async Task UpdateImageForKeyAsync(int keyIndex)
+        {
+            var imageBytes = await _streamDeckApi.GetImageForKeyAsync(keyIndex);
+            using (var ms = new MemoryStream(imageBytes))
+            {
+                _streamDeckController.SetImage(keyIndex, ms);
+            }
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken cancellationToken)
+        {
+            //var bytes = new byte[3];
+            //_random.NextBytes(bytes);
+            //_streamDeckController.FillColor(i, bytes[0], bytes[1], bytes[2]);
+
+            _streamDeckController.FillAllKeysWithColor(0, 255, 0);
+
+            try
+            {
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    var tasks = new List<Task>();
+                    for (int i = 0; i < _streamDeckController.NumKeys; i++)
+                    {
+                        tasks.Add(UpdateImageForKeyAsync(i));
+                    }
+
+                    await Task.WhenAll(tasks.ToArray());
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failure calling GetImageForKey");
+                throw;
+            }
+            finally
+            {
+                _streamDeckController.ClearAllKeys();
+            }
         }
     }
 }
